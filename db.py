b00e230c7862312b07cf4f6df1bd7d2d22921cc3
Swagnik Dutta <swagnikd@gmail.com>
2024-07-24 22:23:06 +0530

    refactor: updated message table and wrote getter functions for messages,rules

--- db.py
+++ db.py
@@ -2,18 +2,23 @@ import json
 import logging
 import sqlite3
 
+from models.message import Message
+from models.rules import Rule
+
 DATABASE_NAME = "store.db"
 
 CREATE_MESSAGES_TABLE = """
 CREATE TABLE IF NOT EXISTS messages (
     id TEXT PRIMARY KEY, 
-    subject TEXT,
     sender TEXT, 
     receiver TEXT, 
+    subject TEXT,
+    body TEXT,
     datetime TEXT
 )
 """
-INSERT_INTO_MESSAGES = "INSERT INTO messages (id, subject, sender, receiver, datetime) VALUES (?, ?, ?, ?, ?)"
+INSERT_INTO_MESSAGES = "INSERT INTO messages (id, sender, receiver, subject, body, datetime) VALUES (?, ?, ?, ?, ?, ?)"
+GET_ALL_MESSAGES = "SELECT * FROM messages"
 
 CREATE_RULES_TABLE = """
 CREATE TABLE IF NOT EXISTS rules (
@@ -24,6 +29,7 @@ CREATE TABLE IF NOT EXISTS rules (
 )
 """
 INSERT_INTO_RULES = "INSERT INTO rules (apply_predicate, conditions, actions) VALUES (?, ?, ?)"
+GET_ALL_RULES = "SELECT * FROM rules"
 
 logger = logging.getLogger(__name__)
 
@@ -58,10 +64,32 @@ def insert_message(conn, message):
     try:
         cursor = conn.cursor()
         cursor.execute(INSERT_INTO_MESSAGES,
-                       (message.id, message.subject, message.sender, message.receiver, message.datetime))
+                       (message.id, message.sender, message.receiver, message.subject, message.body, message.datetime))
         conn.commit()
     except sqlite3.Error as e:
         logger.error(f"Error inserting record into table 'messages'")
+
+
+def get_all_messages(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(GET_ALL_MESSAGES)
+        rows = cursor.fetchall()
+        results = []
+
+        for row in rows:
+            results.append(Message(
+                id=row[0],
+                sender=row[1],
+                receiver=row[2],
+                subject=row[3],
+                body=row[4],
+                datetime=row[5],
+            ))
+        return results
+
+    except sqlite3.Error as e:
+        logger.error(f"Error getting messages. Error: {e}")
 
 
 def insert_rule(conn, rule):
@@ -73,3 +101,22 @@ def insert_rule(conn, rule):
         conn.commit()
     except sqlite3.Error as e:
         logger.error(f"Error inserting record into table: 'rule'")
+
+
+def get_all_rules(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(GET_ALL_RULES)
+        rows = cursor.fetchall()
+        results = []
+
+        for row in rows:
+            results.append(Rule(
+                apply_predicate=row[1],
+                conditions=json.loads(row[2]),
+                actions=json.loads(row[3])
+            ))
+        return results
+
+    except sqlite3.Error as e:
+        logger.error(f"Error getting rules. Error: {e}")
