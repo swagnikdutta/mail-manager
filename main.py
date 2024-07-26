4053d4600e39a2944201ac9958eb6a9390aec237
Swagnik Dutta <swagnikd@gmail.com>
2024-07-26 17:45:00 +0530

    fix: fixed apply rules logic after changes in rule schema

--- main.py
+++ main.py
@@ -5,29 +5,30 @@ from googleapiclient.discovery import build
 
 from api.gmail_api import list_messages, modify_message
 from auth.authenticate import authenticate
-from database.db import setup, insert_rule, get_all_messages, get_all_rules, insert_message
+from database import db
+from models.constants import LABEL_UNREAD, LABEL_INBOX
 from models.rules import Rule
 from models import constants
 
 
 def run():
-    conn = setup()
+    conn = db.setup()
     if not conn:
         return
 
     creds = authenticate()
     svc = build("gmail", "v1", credentials=creds)
 
+    # Processing rules and putting them into database
+    process_rules(conn)
+
     # Fetching messages
-    message_count = 20
+    message_count = 10
     messages = list_messages(svc, message_count)
 
     # Putting messages into database
     for msg in messages:
-        insert_message(conn, msg)
-
-    # Processing rules and putting them into database
-    process_rules(conn)
+        db.insert_message(conn, msg)
 
     # Applying rules on messages
     apply_rules(conn, svc)
@@ -40,51 +41,51 @@ def process_rules(conn):
     with open("rules.json") as rules_file:
         rules = json.load(rules_file)
 
-    # TODO: add validation on field values
     for rule in rules:
         rule_obj = Rule().deserialize(rule)
-        insert_rule(conn, rule_obj)
+        db.insert_rule(conn, rule_obj)
 
 
 def apply_rules(conn, svc):
     logger = logging.getLogger(__name__)
-    messages = get_all_messages(conn)
-    rules = get_all_rules(conn)
+    messages = db.get_all_messages(conn)
+    rules = db.get_all_rules(conn)
 
     for m in messages:
         message_id = m.id
 
         for r in rules:
-            rule_applies = False if r.apply_predicate == constants.ANY else True
+            rule_applies = False if r.conditions.apply_predicate == constants.ANY else True
 
-            for c in r.conditions:
-                match r.apply_predicate:
+            for ci in r.conditions.condition_items:
+                match r.conditions.apply_predicate:
                     case constants.ALL:
-                        rule_applies = rule_applies and m.satisfies_condition(c)
+                        rule_applies = rule_applies and m.satisfies_condition(ci)
                     case constants.ANY:
-                        rule_applies = rule_applies or m.satisfies_condition(c)
+                        rule_applies = rule_applies or m.satisfies_condition(ci)
 
             if rule_applies:
                 logger.info(f"Rule applicable on message: {message_id}")
 
                 for a in r.actions:
                     payload = {}
-                    match a.name:
+                    match a.field:
                         case constants.MARK_AS_READ:
                             payload = {
                                 "addLabelIds": [],
-                                "removeLabelIds": ["UNREAD"]
+                                "removeLabelIds": [LABEL_UNREAD]
                             }
                         case constants.MARK_AS_UNREAD:
                             payload = {
-                                "addLabelIds": ["UNREAD"],
+                                "addLabelIds": [LABEL_UNREAD],
                                 "removeLabelIds": []
                             }
                             modify_message(svc, message_id, payload)
                         case constants.MOVE_MESSAGE:
+                            target = a.predicate
                             payload = {
-                                "addLabelIds": [a.predicate],
-                                "removeLabelIds": ["INBOX"]
+                                "addLabelIds": [target],
+                                "removeLabelIds": [LABEL_INBOX]
                             }
                     modify_message(svc, message_id, payload)
                 # end for
@@ -94,6 +95,7 @@ def apply_rules(conn, svc):
 
 
 def teardown(conn):
+    db.clean(conn)
     conn.close()
 
 
